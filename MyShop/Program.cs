a2131aed84269a4c3ea0aeb64eea79806d31e674
GisleGaren <89572928+GisleGaren@users.noreply.github.com>
2023-09-04 17:07:20 +0200

    Add project files.

---
+++ MyShop/Program.cs
@@ -0,0 +1,11 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// We need the app.useStaticFiles() method middleware in order to use the static files in wwwroot
+app.UseStaticFiles();
+app.MapDefaultControllerRoute();
+
+app.Run();
